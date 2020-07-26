4f160a642ce4ac1e63736b92b61e8f4f8ea80056
VS-coding <64704291+VS-coding@users.noreply.github.com>
2020-07-26 23:31:53 +0000

    Add vote function

--- runoff.c
+++ runoff.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max voters and candidates
 #define MAX_VOTERS 100
@@ -127,6 +128,16 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(candidates[i].name, name) ==0)
+        {
+            printf("Found\n");
+            preferences[voter][rank] = i;
+            return true;
+    
+        }
+    }
     // TODO
     return false;
 }
